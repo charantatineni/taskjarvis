62bd3f1df7a785962ad2c54a42c40faa6db753b1
tatinenisri <tatinenispc@gmail.com>
2025-09-13 22:32:28 -0400

    Initial Commit

---
+++ TaskJarvis/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TaskJarvis
+//
+//  Created by Charan Tatineni on 9/13/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TaskJarvis/TaskJarvisApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskJarvisApp.swift
+//  TaskJarvis
+//
+//  Created by Charan Tatineni on 9/13/25.
+//
+
+import SwiftUI
+
+@main
+struct TaskJarvisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
